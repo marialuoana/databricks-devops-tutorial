2f4c0fbeff371a9284058ed28b7a81cff4356065
marialuoana <47851559+marialuoana@users.noreply.github.com>
2020-09-18 14:40:13 +0100

    'group deployment' deprecated, change to 'deployment group'
    warning when running pipe: 'group deployment' deprecated, change to 'deployment group'

--- azure-pipelines/deploy-resources.sh
+++ azure-pipelines/deploy-resources.sh
@@ -6,5 +6,5 @@ SvcPrincipalObjectId=$(az ad sp show --id $SvcPrincipalApplicationId | jq -r .ob
 
 
 az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
-az group deployment create -g $RESOURCE_GROUP_NAME --template-file arm-templates/azuredeploy.json --parameters dataFactoryName=$DATA_FACTORY_NAME keyVaultName=$KEY_VAULT_NAME storageAccountName=$STORAGE_ACCOUNT_NAME keyVaultWriterPrincipalId=$SvcPrincipalObjectId
+az deployment group create -g $RESOURCE_GROUP_NAME --template-file arm-templates/azuredeploy.json --parameters dataFactoryName=$DATA_FACTORY_NAME keyVaultName=$KEY_VAULT_NAME storageAccountName=$STORAGE_ACCOUNT_NAME keyVaultWriterPrincipalId=$SvcPrincipalObjectId
 
